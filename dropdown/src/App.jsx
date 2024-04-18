ed46b657dc90e007e3d1e94fe8e616e4ba5e624b
Wara Pacajes <wkpacajesm@gmail.com>
2024-04-17 21:10:50 -0400

    se termino con el css

--- dropdown/src/App.jsx
+++ dropdown/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import { Container } from './components/Container.jsx';
+/* import { Search } from './components/Search.jsx'; */
 
 function App() {
   return (
     <div>
+      <h1>Realiza tu busqueda segun el codigo</h1>
+      <textarea
+      className='text'
+       placeholder="Ingrese solo números"
+      ></textarea>
       <Container />
-      <h4>Realizado por Wara Pacajes</h4>
+      <footer>Realizado por Wara Pacajes</footer>
     </div>
   );
 }
